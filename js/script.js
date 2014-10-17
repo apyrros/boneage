090250fc5c705077d11326bc366b8063a9be6020
Patrick Do <patrickdo@gmail.com>
2014-10-16 22:34:14 -0700

    left div

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
+var boneage = {};
+
 $(document).ready(function() {
+	$('#divMalePics').css({'display': 'none'});
+	$(".owl-carousel").owlCarousel();
 
 
 
 
 
 
+	boneage.reset = function() {
+		$('#spanGirl').css({
+			'font-weight': 300,
+			'color': 'white',
+			'text-shadow': '0 0 0'
+		});
 
+		$('#spanBoy').css({
+			'font-weight': 300,
+			'color': 'white',
+			'text-shadow': '0 0 0'
+		});
+	};
 
-
-
-
+	boneage.selectAll = function() {
+		document.getElementById('taReport').focus();
+		document.execCommand('SelectAll');
+	};
 
 	$('#btnBoy').click(function() {
 		$('#spanBoy').css({
 			'font-weight': 'bold',
 			'color': 'white',
-			'text-shadow': '0 0 0 rgba(255, 255, 255, 1)'
+			'text-shadow': '0 0 0'
 		});
 
 		$('#spanGirl').css({
 			'font-weight': 'normal',
 			'color': 'transparent',
-			'text-shadow': '0 0 2px rgba(255, 255, 255, 1)'
+			'text-shadow': '0 0 2px rgba(200, 200, 200, 1)'
 		});
 	});
 
@@ -29,15 +46,34 @@ $(document).ready(function() {
 		$('#spanGirl').css({
 			'font-weight': 'bold',
 			'color': 'white',
-			'text-shadow': '0 0 0 rgba(255, 255, 255, 1)'
+			'text-shadow': '0 0 0'
 		});
 
 		$('#spanBoy').css({
 			'font-weight': 'normal',
 			'color': 'transparent',
-			'text-shadow': '0 0 2px rgba(255, 255, 255, 1)'
+			'text-shadow': '0 0 2px rgba(200, 200, 200, 1)'
 		});
 	});
 
+	$('#labelReport').click(function() {
+		boneage.selectAll();
+	});
+
+	$('#labelDOB').click(function() {
+		$('#inputDOB').select();
+	});
+
+	$('#btnSelectAll').click(function() {
+		boneage.selectAll();
+	});
+
+	$('#btnReset').click(function() {
+		boneage.reset();
+	});
+
+	$('#labelSex').click(function() {
+
+	});
 
 });
