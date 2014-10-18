6bb1fd03785a460989fdfbd17bae29910770166e
Patrick Do <patrickdo@gmail.com>
2014-10-18 09:11:40 -0700

    make images not draggable

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 		mouseDragging: 1,
 		touchDragging: 1,
 		releaseSwing: 1,
-		startAt: 3,
+		startAt: 0,
 		scrollBar: $wrap.find('.scrollbar'),
 		scrollBy: 1,
 		pagesBar: $wrap.find('.pages'),
